b552630310079db5bde0f59f541dab60f0b89ecb
marianbitsov23 <marianbitsov23@gmail.com>
2020-05-21 01:09:45 +0300

    Created home page

--- dist/js/dropdown-menu.js
+++ dist/js/dropdown-menu.js
@@ -1,13 +1,18 @@
 /*! project-name v0.0.1 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 const showDropdown = (element) => {
     const dropdown = element.nextElementSibling;
+    const arrowDown = document.querySelector('.fa-arrow-down');
+    const arrowUp = document.querySelector('.fa-arrow-up');
 
     if(dropdown.style.display === 'none' || dropdown.style.display === '') {
         dropdown.style.display = 'flex';
         dropdown.style.flexDirection = 'column';
-        element.style.borderBottom = '1px solid #8d8d8d';
+        arrowDown.style.display = 'none';
+        arrowUp.style.display = 'inline-block';
     } else {
         dropdown.style.display = 'none';
         element.style.borderBottom = 'none';
+        arrowDown.style.display = 'inline-block';
+        arrowUp.style.display = 'none';
     }
 }
--- src/js/dropdown-menu.js
+++ src/js/dropdown-menu.js
@@ -1,12 +1,17 @@
 const showDropdown = (element) => {
     const dropdown = element.nextElementSibling;
+    const arrowDown = document.querySelector('.fa-arrow-down');
+    const arrowUp = document.querySelector('.fa-arrow-up');
 
     if(dropdown.style.display === 'none' || dropdown.style.display === '') {
         dropdown.style.display = 'flex';
         dropdown.style.flexDirection = 'column';
-        element.style.borderBottom = '1px solid #8d8d8d';
+        arrowDown.style.display = 'none';
+        arrowUp.style.display = 'inline-block';
     } else {
         dropdown.style.display = 'none';
         element.style.borderBottom = 'none';
+        arrowDown.style.display = 'inline-block';
+        arrowUp.style.display = 'none';
     }
 }
